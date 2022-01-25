17794b8398e169dbf2d71b88542bdfd7a6f2a437
sbarreiros <silvino.barreiros@gmail.com>
2022-01-24 21:55:43 -0500

    more links + phots

--- src/images.js
+++ src/images.js
@@ -10,6 +10,19 @@ import jacksonhole from './Assets/jacksonhole.jpg'
 import matterhorn from './Assets/matterhorn.jpg'
 import milian from './Assets/milian.jpg'
 import netflix from './Assets/netflix.png'
+import bar1 from './Assets/bar_1.jpg'
+import bigSky1 from './Assets/big_sky_1.jpg'
+import bigSky2 from './Assets/big_sky_2.jpg'
+import covidLife from './Assets/covid_life.jpg'
+import covidLife2 from './Assets/covid_life_2.jpg'
+import dolphins1 from './Assets/dolphins_1.jpg'
+import france1 from './Assets/france_1.jpg'
+import jacksonhole2 from './Assets/jacksonhole_2.jpg'
+import montreal from './Assets/montreal.jpg'
+import wedding1 from './Assets/wedding_1.jpg'
+import wedding2 from './Assets/wedding_2.jpg'
+import wedding3 from './Assets/wedding_3.jpg'
+import zermont from './Assets/zermont.jpg'
 
 export const photos = {
   beachOne: { url: beachOne, description: 'ring on the beach' },
@@ -23,7 +36,20 @@ export const photos = {
   jacksonhole: { url: jacksonhole, description: 'jackson hole' },
   matterhorn: { url: matterhorn, description: 'big mountain' },
   milian: { url: milian, description: 'sky daddy house' },
-  netflix: { url: netflix, description: 'we famous' }
+  netflix: { url: netflix, description: 'we famous' },
+  bar1: { url: bar1, description: 'bar1' },
+  bigSky1: { url: bigSky1, description: 'bigSky1' },
+  bigSky2: { url: bigSky2, description: 'bigSky2' },
+  covidLife: { url: covidLife, description: 'covidLife' },
+  covidLife2: { url: covidLife2, description: 'covidLife2' },
+  dolphins1: { url: dolphins1, description: 'dolphins1' },
+  france1: { url: france1, description: 'france1' },
+  jacksonhole2: { url: jacksonhole2, description: 'jacksonhole2' },
+  montreal: { url: montreal, description: 'montreal' },
+  wedding1: { url: wedding1, description: 'wedding1' },
+  wedding2: { url: wedding2, description: 'wedding2' },
+  wedding3: { url: wedding3, description: 'wedding3' },
+  zermont: { url: zermont, description: 'zermont' }
 }
 
 export const coverPhotos = [photos.brick, photos.beachFive]
@@ -41,7 +67,20 @@ export const otherPhotos = [
   photos.jacksonhole,
   photos.matterhorn,
   photos.milian,
-  photos.netflix
+  photos.netflix,
+  photos.bar1,
+  photos.bigSky1,
+  photos.bigSky2,
+  photos.covidLife,
+  photos.covidLife2,
+  photos.dolphins1,
+  photos.france1,
+  photos.jacksonhole2,
+  photos.montreal,
+  photos.wedding1,
+  photos.wedding2,
+  photos.wedding3,
+  photos.zermont
 ]
 
 export const allExceptCover = [...beachPhotos, ...otherPhotos]
