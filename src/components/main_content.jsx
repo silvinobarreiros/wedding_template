d9c589dee7724d60dc51e9369913ad2769cf5374
MaryAlexa Divver <maryalexa@users.noreply.github.com>
2022-02-28 08:59:07 -0700

    remove vaccine mandate info

--- src/components/main_content.jsx
+++ src/components/main_content.jsx
@@ -95,13 +95,12 @@ const MainContent = ({ open }) => (
                     rel="noopener"
                     aria-label="Ravel Hotel"
                   >
-                    Ravel Hotel*
+                    Ravel Hotel
                   </a>
                   <br></br>
                   <br></br>
                   <i id="ceremony">
-                    *Current NYC mandates require proof of Covid-19 vaccination
-                    for all guests 13 years and older.
+                    
                   </i>
                 </p>
               </div>
