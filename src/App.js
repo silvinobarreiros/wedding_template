74b949091a05c8c7aa9253e8514238599c362c12
sbarreiros <silvino.barreiros@gmail.com>
2022-05-07 23:04:48 -0400

    mobile friendly 2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import './App.css'
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
 import DrawerMenu from './components/drawer'
-import NavBar from './components/navbar'
 import MainContent from './components/main_content'
 
 function App() {
   const drawerWidth = 240
   const [open, setOpen] = React.useState(false)
-
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
 
   const handleDrawerClose = () => {
     setOpen(false)
@@ -21,11 +16,6 @@ function App() {
   return (
     <Box sx={{ display: 'flex' }} id="parent">
       <CssBaseline />
-      <NavBar
-        handleDrawerOpen={handleDrawerOpen}
-        open={open}
-        drawerWidth={drawerWidth}
-      />
       <MainContent drawerWidth={drawerWidth} open={open} />
       <DrawerMenu
         drawerWidth={drawerWidth}
--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -1,54 +1,57 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { styled } from '@mui/material/styles'
-
-import { getRandomEntry } from '../util'
+import useWindowSize from '../hooks/useWindowSize'
+import { getRandomEntry, isMobile } from '../util'
 
 // eslint-disable-next-line no-empty-pattern
-const RootStyle = styled('div')(({}) => ({
-  position: 'relative',
-  width: '100%',
-  height: '100%',
-  h1: {
+const RootStyle = styled('div')(({ size }) => {
+  return {
     position: 'relative',
-    textAlign: 'center',
-    justifyContent: 'center',
-    zIndex: 2,
-    top: '-60%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    fontSize: '6rem',
-    fontFamily: 'Dancing Script, cursive'
-  },
-  h2: {
-    position: 'relative',
-    textAlign: 'center',
-    justifyContent: 'center',
-    zIndex: 2,
-    top: '-70%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    fontSize: '2rem',
-    fontFamily: 'Dancing Script, cursive'
-  },
-  img: {
-    objectFit: 'cover',
     width: '100%',
     height: '100%',
-    position: 'relative',
-    top: 0,
-    left: 0,
-    zIndex: 1,
-    opacity: 0.4
+    h1: {
+      position: 'relative',
+      textAlign: 'center',
+      justifyContent: 'center',
+      zIndex: 2,
+      top: '-70%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      fontSize: isMobile(size) ? '4rem' : '6rem',
+      fontFamily: 'Dancing Script, cursive'
+    },
+    h2: {
+      position: 'relative',
+      textAlign: 'center',
+      justifyContent: 'center',
+      zIndex: 2,
+      top: '-70%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      fontSize: '2rem',
+      fontFamily: 'Dancing Script, cursive'
+    },
+    img: {
+      objectFit: 'cover',
+      width: '100%',
+      height: '100%',
+      position: 'relative',
+      top: 0,
+      left: 0,
+      zIndex: 1,
+      opacity: 0.4
+    }
   }
-}))
+})
 
 const Jumbotron = ({ pics }) => {
+  const size = useWindowSize()
   const randomPic = getRandomEntry(pics)
   const [pic] = useState(randomPic)
 
   return (
-    <RootStyle>
+    <RootStyle size={size}>
       <img src={pic.url} alt={pic.description} />
       <h1> MaryAlexa & Silvino </h1>
       <h2> 10.08.2022 </h2>
