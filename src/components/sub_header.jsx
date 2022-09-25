d49e3ddfa2ba84b3670524a479586d7ae70ffd7f
sbarreiros <silvino.barreiros@gmail.com>
2022-09-25 12:49:13 -0400

    adjust font sizes for mobile

--- src/components/sub_header.jsx
+++ src/components/sub_header.jsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { styled } from '@mui/material/styles'
+import useWindowSize from '../hooks/useWindowSize'
+
+import { isMobile } from '../util'
 
 // eslint-disable-next-line no-empty-pattern
-const RootStyle = styled('div')(({}) => ({
-  fontFamily: 'ArtisanSignature',
-  textAlign: 'center',
-  h2: { fontSize: '3rem', marginTop: '3rem', marginBottom: '25px' },
-  div: { fontSize: '1.5rem', marginBottom: '2px' }
-}))
+const RootStyle = styled('div')(({ size }) => {
+  const base = {
+    fontFamily: 'ArtisanSignature',
+    textAlign: 'center'
+  }
 
-const SubHeader = ({ title, subTitle }) => (
-  <RootStyle id={title === 'Our Story' ? 'ourstory' : `${title}`}>
-    <h2>{title}</h2>
-    {subTitle !== undefined && <div>{subTitle}</div>}
-  </RootStyle>
-)
+  if (isMobile(size)) {
+    return {
+      ...base,
+      h2: { fontSize: '2.5rem', marginTop: '3rem', marginBottom: '25px' },
+      div: { fontSize: '1rem', marginBottom: '2px' }
+    }
+  }
+
+  return {
+    ...base,
+    h2: { fontSize: '3rem', marginTop: '3rem', marginBottom: '25px' },
+    div: { fontSize: '1.5rem', marginBottom: '2px' }
+  }
+})
+
+const SubHeader = ({ title, subTitle }) => {
+  const size = useWindowSize()
+
+  return (
+    <RootStyle id={title === 'Our Story' ? 'ourstory' : `${title}`} size={size}>
+      <h2>{title}</h2>
+      {subTitle !== undefined && <div>{subTitle}</div>}
+    </RootStyle>
+  )
+}
 
 SubHeader.propTypes = {
   title: PropTypes.string,
